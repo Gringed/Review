74e53ecd461c874297023ad68641266972100767
Alexandre Guillome <alexandre1906@live.fr>
2024-02-14 14:49:32 +0100

    fix() stripe plan support

--- components/modals/pro-modal.tsx
+++ components/modals/pro-modal.tsx
@@ -13,12 +13,13 @@ import { Organization } from "@prisma/client";
 import { fetcher } from "@/lib/fetcher";
 import { useParams } from "next/navigation";
 import { Separator } from "../ui/separator";
+import { AlertCircle, FileWarning } from "lucide-react";
 
 export const ProModal = () => {
   const id = useProModal((state) => state.id);
   const isOpen = useProModal((state) => state.isOpen);
   const onClose = useProModal((state) => state.onClose);
-  const params = useParams()
+  const params = useParams();
   const { data: org } = useQuery<Organization[]>({
     queryKey: ["organization", id],
     queryFn: () => fetcher(`/api/organization/${id}`),
@@ -29,38 +30,33 @@ export const ProModal = () => {
     },
     onError: (error) => {
       toast.error(error);
-    }
+    },
   });
   const onClick = () => {
-    execute({orgId: params.organizationId as string});
+    execute({ orgId: params.organizationId as string });
   };
-  
+
   return (
-    <Dialog
-      open={isOpen}
-      onOpenChange={onClose}
-    >
+    <Dialog open={isOpen} onOpenChange={onClose}>
       <DialogContent className="max-w-md p-0 overflow-hidden">
         <div className=" aspect-video relative ">
-          <Image
-            src="/hero.svg"
-            alt="Hero"
-            className="object-contain"
-            fill
-          />
+          <Image src="/hero.svg" alt="Hero" className="object-contain" fill />
         </div>
         <div className="text-neutral-700 mx-auto space-y-6 p-6">
-          <h2 className="font-semibold text-xl">
+          <h2 className="font-semibold text-xl text-center">
             Upgrade to Review Pro Today!
           </h2>
-          <p className="text-xs font-semibold text-neutral-600">
-            Explore the best of Review
-          </p>
+          <div
+            className="mb-4 p-5 flex items-center gap-2 rounded-lg bg-orange-100  text-xs font-semibold text-orange-400"
+            role="alert"
+          >
+           <AlertCircle /> For now, this plan support me, any updates comes
+          </div>
           <div className="pl-3">
             <ul className="text-sm list-disc">
               <li>Unlimited boards</li>
               <li>Create a team environment</li>
-              <li>Admin and security features</li>
+              <li>Unlimited Team members</li>
               <li>And more!</li>
             </ul>
           </div>
